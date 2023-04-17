925e64a3d53e74b3286e12f125ceb6bb0f1d8eac
SvMyko <130339482+SvMyko@users.noreply.github.com>
2023-04-17 10:39:42 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clean_folder',
+    version='1.0',
+    author='SVT',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'clean_folder = clean_folder.Sort:main']
+    },
+    install_requires=[
+        'shutil',
+    ],
+)
